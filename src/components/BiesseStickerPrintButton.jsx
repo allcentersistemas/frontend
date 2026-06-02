809e6d94cceb4eabfeb88f78256ad74df84f97a1
arturoruizromero <sistemas@allcenter.pe>
2026-06-02 18:32:26 -0500

    hola

--- src/components/BiesseStickerPrintButton.jsx
+++ src/components/BiesseStickerPrintButton.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useState } from 'react'
-import { openStickerPrintWindow, printBiessePartSticker } from '../utils/printBiessePartSticker'
 import * as systemApi from '../api/systemApi'
 import { Button } from '../ui/Button.jsx'
 import { InlineCode } from '../ui/InlineCode.jsx'
